9a7059a7b3ef5aaf1a5d2a37d2b609151d6c5f25
kush148 <kushal148@live.com>
2020-12-25 16:16:59 -0500

    coroutines networktimeout demo added

--- app/src/main/java/kush/example/kotlincoroutinesexapmle/MainActivity.kt
+++ app/src/main/java/kush/example/kotlincoroutinesexapmle/MainActivity.kt
@@ -4,17 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private val RESULT1 = "result1"
     private val RESULT2 = "result2"
+    private val NETWORK_TIMEOUT = 500L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,10 +26,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     suspend fun setTextOnMainThread(result: String) {
+
         withContext(Main) {
             tv.text = result
+
+            val job = withTimeoutOrNull(NETWORK_TIMEOUT) {
+                val result2 = getResult2FromApi()
+                withContext(Main) {
+                    tv2.text = result2
+                }
+            }
+            if (job == null) {
+                tv2.text = "Process took too long to execute !!"
+            }
         }
     }
+
+//    private suspend fun fakeApiRequest() {
+//        val result = getResult1FromApi()
+//        setTextOnMainThread(result)
+//    }
 
     suspend fun getResult1FromApi(): String {
         delay(1000)
